026cd49fd7bab3ed56aa25da38a7f8826357303b
Massimo Biagioli <biagiolimassimo@gmail.com>
2020-02-17 00:08:20 +0100

    Impostazione routes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from './components/App';
-//import vuetify from 'vuetify';
-//import router from './router';
-//import store from './store';
+import vuetify from 'vuetify';
+import router from './router';
+import store from './store';
 
 new Vue({
-  el: '#app',
+  vuetify,
+  router,
+  store,
   render: h => h(App)
-});
+}).$mount('#app');
--- assets/js/components/App.vue
+++ assets/js/components/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>Home</div>
+  <router-view></router-view>
 </template>
 
 <script>
 export default {
+  mounted() {
+    let root = "/login";
+    this.$router.push({ path: root });
+  }
 };
 </script>
