33ed04d7018e58cbbe44b0c2988753f418205a82
Massimo Biagioli <biagiolimassimo@gmail.com>
2020-02-16 23:31:54 +0100

    vuetify router

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import App from './components/App';
+//import vuetify from 'vuetify';
+//import router from './router';
+//import store from './store';
 
 new Vue({
-    el: '#app',
-    render: h => h(App)
+  el: '#app',
+  render: h => h(App)
 });
---
+++ assets/js/router.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './components/Login.vue'
+import Admin from './components/Admin.vue'
+import User from './components/User.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }, {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin
+    }, {
+      path: '/user',
+      name: 'User',
+      component: User
+    }
+  ]
+})
