0370090db6a7df9b2bc98bf2c43531032a4f0348
Massimo Biagioli <m.biagioli@palinformatica.it>
2020-02-18 14:30:19 +0100

    API Client

--- assets/js/lib/baratto-api-client.js
+++ assets/js/lib/baratto-api-client.js
@@ -1,4 +1,5 @@
 class BarattoApiClient {
+  
   async login(email, password) {
     let options = {
       method: 'POST',
@@ -16,6 +17,94 @@ class BarattoApiClient {
       return null;
     }
   }
+
+  async listArticoli(authToken) {
+    let headers = new Headers();
+    headers.append('X-AUTH-TOKEN', $authToken);
+    let options = {
+      method: 'GET',
+      headers
+    };
+    try {
+      let response = await fetch('/api/admin/articoli', options);
+      let data = await response.json();
+      return data;
+    } catch (err) {
+      console.log(err.message);
+      return null;
+    }
+  }
+
+  async getArticolo(authToken, id) {
+    let headers = new Headers();
+    headers.append('X-AUTH-TOKEN', $authToken);
+    let options = {
+      method: 'GET',
+      headers
+    };
+    try {
+      let response = await fetch(`/api/admin/articoli/${id}`, options);
+      let data = await response.json();
+      return data;
+    } catch (err) {
+      console.log(err.message);
+      return null;
+    }
+  }
+
+  async insertArticolo(authToken, articolo) {
+    let headers = new Headers();
+    headers.append('X-AUTH-TOKEN', $authToken);
+    let options = {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(articolo)
+    };
+    try {
+      let response = await fetch('/api/admin/articoli', options);
+      let data = await response.json();
+      return data;
+    } catch (err) {
+      console.log(err.message);
+      return null;
+    }
+  }
+
+  async updateArticolo(authToken, id, articolo) {
+    let headers = new Headers();
+    headers.append('X-AUTH-TOKEN', $authToken);
+    let options = {
+      method: 'UPDATE',
+      headers,
+      body: JSON.stringify(articolo)
+    };
+    try {
+      let response = await fetch(`/api/admin/articoli/${id}`, options);
+      let data = await response.json();
+      return data;
+    } catch (err) {
+      console.log(err.message);
+      return null;
+    }
+  }
+
+  async deleteArticolo(authToken, id) {
+    let headers = new Headers();
+    headers.append('X-AUTH-TOKEN', $authToken);
+    let options = {
+      method: 'DELETE',
+      headers
+    };
+    try {
+      let response = await fetch(`/api/admin/articoli/${id}`, options);
+      let data = await response.json();
+      return data;
+    } catch (err) {
+      console.log(err.message);
+      return null;
+    }
+  }
+
 }
 
 export default BarattoApiClient;
