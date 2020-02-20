9dad712ee50bf1b1163fa251b23f43e542d50a22
Massimo Biagioli <m.biagioli@palinformatica.it>
2020-02-20 12:08:41 +0100

    Implementazione nuovi metodi buy, listItemsToBuy, listPurchased

--- src/Service/User/UserDoctrineImpl.php
+++ src/Service/User/UserDoctrineImpl.php
@@ -6,6 +6,7 @@ use App\Entity\Articolo;
 use App\Entity\Movimento;
 use App\Entity\Utente;
 use App\Service\User\UserInterface;
+use App\Service\User\Ticket;
 use Doctrine\ORM\EntityManagerInterface;
 
 final class UserDoctrineImpl implements UserInterface
@@ -48,6 +49,47 @@ final class UserDoctrineImpl implements UserInterface
         $this->entityManager->flush();
     }
 
+    public function buy(int $movimentoId, int $articoloId, int $compratoreId): Ticket
+    {
+        $compratore = $this->utenteRepository->find($compratoreId);
+        if (!$compratore) {
+            throw new \Exception("Venditore non trovato");
+        }       
+        $articolo = $this->articoloRepository->find($articoloId);
+        if (!$articolo) {
+            throw new \Exception("Articolo non trovato");
+        }
+        $movimentoVendita = $this->movimentoRepository->find($movimentoId);
+        if (!$movimentoVendita) {
+            throw new \Exception("Movimento vendita non trovato");
+        }
+        
+        $totalPrice = $articolo->getMonete() * $quantita;
+        if ($utente->getMonete() < $totalPrice) {
+            throw new \Exception("Disponibilità monete insufficiente");
+        }
+
+        $movimento = new Movimento();
+        $movimento->setArticolo($articolo);
+        $movimento->setVenditore($movimentoVendita->getVenditore());
+        $movimento->setQuantita($quantita);
+        $movimento->setDataOperazione(\DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s')));
+        $movimento->setTipo(Movimento::TIPO_ACQUISTO);
+        $movimento->setStato(Movimento::STATO_ACQUISTATO);
+        $ticket = new Ticket();
+        $movimento->setTicket($ticket->getValue());
+        $movimento->setIdMovimentoVendita($movimentoId);
+        $this->validateMovimento($movimento);
+        $this->entityManager->persist($movimento);
+        
+        $compratore->setMonete($utente->getMonete() - $totalPrice);
+        $this->entityManager->persist($compratore);
+
+        $this->entityManager->flush();        
+
+        return $ticket;
+    }
+
     public function listItemsForSale(int $utenteId): array
     {
         $criteria = [
@@ -60,6 +102,33 @@ final class UserDoctrineImpl implements UserInterface
         });
         return $movimenti;
     }
+
+    public function listItemsToBuy(int $utenteId): array
+    {
+        $criteria = [
+            'tipo' => Movimento::TIPO_VENDITA,
+            'stato' => Movimento::STATO_INSERITO,
+        ];
+        $movimenti = $this->movimentoRepository->findBy($criteria, ['dataOperazione' => 'DESC']);
+        $movimenti = array_filter($movimenti, function ($m) use ($utenteId) {
+            $movimentoAcquisto = $this->movimentoRepository->findBy(['idMovimentoVendita' => $m->getId()]);
+            return !!$movimentoAcquisto;
+        });
+        return $movimenti;
+    }
+
+    public function listItemsPurchased(int $utenteId): array
+    {
+        $criteria = [
+            'tipo' => Movimento::TIPO_ACQUISTO,
+            'stato' => Movimento::STATO_ACQUISTATO,
+        ];
+        $movimenti = $this->movimentoRepository->findBy($criteria, ['dataOperazione' => 'DESC']);
+        $movimenti = array_filter($movimenti, function ($m) use ($utenteId) {
+            return $m->getCompratore()->getId() == $utenteId;
+        });
+        return $movimenti;
+    }    
 
     public function residualCoins(int $utenteId): int
     {
--- src/Service/User/UserInterface.php
+++ src/Service/User/UserInterface.php
@@ -3,10 +3,14 @@
 namespace App\Service\User;
 
 use App\Entity\Movimento;
+use App\Service\User\Ticket;
 
 interface UserInterface
 {
     public function sell(int $articoloId, int $venditoreId, int $quantita): void;
+    public function buy(int $movimentoId, int $articoloId, int $venditoreId): Ticket;
     public function listItemsForSale(int $utenteId): array;
+    public function listItemsToBuySale(int $utenteId): array;
+    public function listItemsPurchased(int $utenteId): array;
     public function residualCoins(int $utenteId): int;
 }
