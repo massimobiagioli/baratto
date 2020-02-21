460b3745eca2324e4cf1ad4d86c393af6ce97cc0
Massimo Biagioli <m.biagioli@palinformatica.it>
2020-02-21 11:56:58 +0100

    Bugfix Unit Test

--- tests/Service/User/UserTest.php
+++ tests/Service/User/UserTest.php
@@ -27,7 +27,7 @@ class UserTest extends TestCase
         $articoli[] = $articolo1;
 
         $articolo2 = new Articolo();
-        $articolo2->setId(1);
+        $articolo2->setId(2);
         $articolo2->setNome('Sviluppo frontend ReactJs (1h)');
         $articolo2->setMonete(10);
         $articoli[] = $articolo2;
@@ -41,30 +41,88 @@ class UserTest extends TestCase
             ->method('find')
             ->will($this->returnValueMap($args));
 
+        $utenti = [];
+
+        $utente1 = new Utente();
+        $utente1->setId(1);
+        $utente1->setEmail('franco.rossi@gmail.com');
+        $utente1->setMonete(100);
+        $utenti[] = $utente1;
+
+        $utente2 = new Utente();
+        $utente2->setId(2);
+        $utente2->setEmail('lucio.neri@gmail.com');
+        $utente2->setMonete(100);
+        $utenti[] = $utente2;
+
+        $utente3 = new Utente();
+        $utente3->setId(3);
+        $utente3->setEmail('piero.verdi@gmail.com');
+        $utente3->setMonete(5);
+        $utenti[] = $utente2;
+
+        $utenteRepository = $this->createMock(UtenteRepository::class);
+        $args = [
+            [1, null, null, $utente1],
+            [2, null, null, $utente2],
+        ];
+        $utenteRepository->expects($this->any())
+            ->method('find')
+            ->will($this->returnValueMap($args));
+    
         $movimenti = [];
 
         $movimento1 = new Movimento();
         $movimento1->setId(1);
+        $movimento1->setArticolo($articolo1);
+        $movimento1->setVenditore($utente1);
+        $movimento1->setQuantita(1);        
+        $movimento1->setTipo(Movimento::TIPO_VENDITA);
+        $movimento1->setStato(Movimento::STATO_INSERITO);
         $movimenti[] = $movimento1;
 
         $movimento2 = new Movimento();
         $movimento2->setId(2);
+        $movimento2->setArticolo($articolo1);
+        $movimento2->setVenditore($utente1);
+        $movimento2->setQuantita(2);
+        $movimento2->setTipo(Movimento::TIPO_VENDITA);
+        $movimento2->setStato(Movimento::STATO_INSERITO);
         $movimenti[] = $movimento2;
 
         $movimento3 = new Movimento();
         $movimento3->setId(3);
+        $movimento3->setArticolo($articolo2);
+        $movimento3->setVenditore($utente1);
+        $movimento3->setQuantita(3);
+        $movimento3->setTipo(Movimento::TIPO_VENDITA);
+        $movimento3->setStato(Movimento::STATO_INSERITO);
         $movimenti[] = $movimento3;
 
         $movimento4 = new Movimento();
         $movimento4->setId(4);
+        $movimento4->setArticolo($articolo2);
+        $movimento4->setVenditore($utente1);
+        $movimento4->setCompratore($utente2);
+        $movimento4->setIdMovimentoVendita(1);
+        $movimento4->setQuantita(3);
+        $movimento4->setTipo(Movimento::TIPO_ACQUISTO);
+        $movimento4->setStato(Movimento::STATO_ACQUISTATO);
         $movimenti[] = $movimento4;
         $forSale = [$movimento1, $movimento2, $movimento3];
         $purchased = [$movimento4];
-
         $movimentoRepository = $this->createMock(MovimentoRepository::class);
         $args = [
-            [['tipo' => Movimento::TIPO_VENDITA, 'stato' => Movimento::STATO_INSERITO], null, null, null, $forSale],
-            [['tipo' => Movimento::TIPO_ACQUISTO, 'stato' => Movimento::STATO_ACQUISTATO], null, null, null, $purchased],
+            [
+                ['tipo' => Movimento::TIPO_VENDITA, 'stato' => Movimento::STATO_INSERITO], 
+                ['dataOperazione' => 'DESC'], 
+                null, null, $forSale
+            ],
+            [
+                ['tipo' => Movimento::TIPO_ACQUISTO, 'stato' => Movimento::STATO_ACQUISTATO], 
+                ['dataOperazione' => 'DESC'],
+                null, null, $purchased
+            ],
         ];
         $movimentoRepository->expects($this->any())
             ->method('findBy')
@@ -77,29 +135,6 @@ class UserTest extends TestCase
               [4, null, null, $movimento4]
         ];
         $movimentoRepository->expects($this->any())
-            ->method('find')
-            ->will($this->returnValueMap($args));
-
-        $utenti = [];
-
-        $utente1 = new Utente();
-        $utente1->setId(1);
-        $utente1->setEmail('franco@gmail.com');
-        $utente1->setMonete(10);
-        $utenti[] = $utente1;
-
-        $utente2 = new Utente();
-        $utente2->setId(1);
-        $utente2->setEmail('lucio@gmail.com');
-        $utente2->setMonete(10);
-        $utenti[] = $utente2;
-
-        $utenteRepository = $this->createMock(UtenteRepository::class);
-        $args = [
-            [1, null, null, $utente1],
-            [2, null, null, $utente2],
-        ];
-        $utenteRepository->expects($this->any())
             ->method('find')
             ->will($this->returnValueMap($args));
 
@@ -136,20 +171,20 @@ class UserTest extends TestCase
     public function test_buy_with_right_availability_returns_a_valid_ticket()
     {
         $movimentoId = 1;
-        $venditoreId = 1;
-        $ticket = $this->userService->buy($movimentoId, $venditoreId);
+        $compratoreId = 1;
+        $ticket = $this->userService->buy($movimentoId, $compratoreId);
         $ret = preg_match('/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/', $ticket->getValue(), $matches);
         $this->assertNotNull($ticket);
         $this->assertEquals(1, $ret);
         $this->assertEquals($ticket->getValue(), $matches[0]);
     }
 
-    public function test_buy_with_wrong_availability_returns_a_valid_ticket()
+    public function test_buy_with_wrong_availability_returns_exception()
     {
         $this->expectException(\Exception::class);
         $movimentoId = 1;
-        $venditoreId = 1;
-        $ticket = $this->userService->buy($movimentoId, $venditoreId);
+        $compratoreId = 3;
+        $ticket = $this->userService->buy($movimentoId, $compratoreId);
     }
 
     public function test_close_returns_void()
@@ -165,28 +200,28 @@ class UserTest extends TestCase
     {
         $utenteId = 1;
         $movimenti = $this->userService->listItemsForSale($utenteId);
-        $this->assertEquals(1, count($movimenti));
+        $this->assertEquals(3, count($movimenti));
     }
 
     public function test_list_residual_coins_returns_the_right_value()
     {
         $utenteId = 1;
         $residualCoins = $this->userService->residualCoins($utenteId);
-        $this->assertEquals(10, $residualCoins);
+        $this->assertEquals(100, $residualCoins);
     }
 
     public function test_list_items_to_buy_returns_the_right_list()
     {
-        $utenteId = 1;
+        $utenteId = 2;
         $movimenti = $this->userService->listItemsToBuy($utenteId);
-        $this->assertEquals(2, count($movimenti));
+        $this->assertEquals(3, count($movimenti));
     }
 
     public function test_list_items_purchased_returns_the_right_list()
     {
         $utenteId = 1;
         $movimenti = $this->userService->listItemsPurchased($utenteId);
-        $this->assertEquals(1, count($movimenti));
+        $this->assertEquals(0, count($movimenti));
     }
 
     public function test_list_items_to_close_returns_the_right_list()
